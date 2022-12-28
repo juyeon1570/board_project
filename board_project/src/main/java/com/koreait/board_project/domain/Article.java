0e76812b23d42ca2b081030fbff2377c6ee5dd4b
juyeon1570 <m15701313@gmail.com>
2022-12-28 11:41:19 +0900

    #8 - 스프링 데이터 JPA 속성과 설정
    * JPA Auditing 기능을 쓰기 위해 config 추가
    * DB 접근과기능 편의를 위한 각종 프로퍼티 연결

--- board_project/src/main/java/com/koreait/board_project/domain/Article.java
+++ board_project/src/main/java/com/koreait/board_project/domain/Article.java
@@ -1,11 +1,20 @@
 package com.koreait.board_project.domain;
 
+import io.micrometer.core.lang.Nullable;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Index;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @ToString
@@ -15,15 +24,54 @@ import java.time.LocalDateTime;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
 })
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Article {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String title;
-    private String content;
-    private String hashtag;
-    private String createdBy;
-    private LocalDateTime createdAt;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    @Setter @Column(nullable = false, length = 10000) private String title;
+    @Setter private String content;
+    @Setter private String hashtag;
+
+    @CreatedDate @Column(nullable = false) private String createdBy;
+    @CreatedBy @Column(nullable = false, length = 100) private LocalDateTime createdAt;
+    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;
+    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;
+
+
+    @ToString.Exclude
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+    protected Article(){}
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj){
+            return true;
+        }
+        if(!(obj instanceof Article article)){
+            return false;
+        }
+         return id != null && id.equals(article.id);
+    }
+
+
 
 }

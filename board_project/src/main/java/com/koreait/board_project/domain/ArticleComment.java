7a082e3bb2f3f70e6ac52a02c1e170bc685327e2
juyeon1570 <m15701313@gmail.com>
2022-12-28 10:35:55 +0900

    Article, ArticleComment 클래스, properties.yml

--- board_project/src/main/java/com/koreait/board_project/domain/ArticleComment.java
+++ board_project/src/main/java/com/koreait/board_project/domain/ArticleComment.java
@@ -1,2 +1,13 @@
-package com.koreait.board_project.domain;public class ArticleComment {
+package com.koreait.board_project.domain;
+
+import java.time.LocalDateTime;
+
+public class ArticleComment {
+    private Long id;
+    private Article article;     // 게시글 아이디
+    private String content;
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
 }
